5b06a206d996e36d2fde57ba9b4265af3dc556fb
Olivier Robert <olivierobert@gmail.com>
2023-10-21 11:39:39 +0200

    List all notes on the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,49 @@
+'use client'
+
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+
 export default function Home() {
+  const [notes, setNotes] = useState([]);
+
+  const getNotes = async () => {
+    try {
+      const response = await fetch('https://challenge.surfe.com/aloha/notes', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      });
+
+      const data = await response.json();
+
+      setNotes(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getNotes();
+  }, []);
+
   return (
-    <></>
-  )
+    <>
+      <header>
+        <Link href="/notes/new">New Note</Link>
+      </header>
+      <main>
+        <h1>Notes</h1>
+        {notes.length === 0 && <p>No notes yet</p>}
+        <ul>
+          {notes.length && notes.map((note) => (
+            <li key={`aloha-note-${note.id}`}>
+              <div>{note.body}</div>
+              <Link href={`/notes/${note.id}`}>Edit</Link>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </>
+  );
 }
