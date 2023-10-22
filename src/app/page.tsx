e5cf1639a55274af27e966e41e72e559b6296e37
Olivier Robert <olivierobert@gmail.com>
2023-10-22 16:25:20 +0200

    [#4] Fetch he list of notes of page load

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,42 @@
+'use client';
+
+import { useEffect, useState } from "react";
+import Link from 'next/link';
+
+import apiClient, { ENDPOINT } from '@/config/api';
+import { Note } from '@/types/note';
+
 export default function Home() {
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  const getNotes = async () => {
+    try {
+      const response = await apiClient.get<Note[]>(ENDPOINT.GET_NOTES);
+
+      setNotes(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getNotes();
+  },[]);
+
   return (
-    <></>
+    <>
+      <main>
+        <h1>Notes</h1>
+        {notes.length === 0 && <p>No notes yet</p>}
+        <ul>
+          {notes.length && notes.map((note) => (
+            <li key={`aloha-note-${note.id}`}>
+              <div>{note.body}</div>
+              <Link href={`/notes/${note.id}`}>Edit</Link>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </>
   )
 }
--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,9 @@
 import { ApiClient } from "@/lib/api/client";
 
+export const ENDPOINT = {
+  GET_NOTES: '/notes',
+};
+
 const apiClient = new ApiClient({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL as string });
 
 export default apiClient;
