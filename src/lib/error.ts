b6780d4cb4745a596d61e1f1cf97e941f2de85bb
Olivier Robert <olivierobert@gmail.com>
2023-10-22 15:26:42 +0200

    [#3] Add a request manager based off fetch

---
+++ src/lib/error.ts
@@ -0,0 +1,11 @@
+class BaseError extends Error {
+  constructor({ message }: { message: string }) {
+    super(message);
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, BaseError);
+    }
+  }
+}
+
+export class RequestError extends BaseError {}
