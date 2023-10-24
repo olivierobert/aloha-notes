0a27790abc95c91cf2980302a28ffcc5fe917409
Olivier Robert <olivierobert@gmail.com>
2023-10-24 12:49:48 +0200

    [#8] Add new page to edit a note

---
+++ src/app/notes/[id]/page.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import React from 'react';
+import { useParams } from 'next/navigation';
+import Link from 'next/link';
+
+import AppHeader from '@/components/AppHeader';
+import AppMain from '@/components/AppMain';
+import NoteEditor from '@/components/NoteEditor';
+import type { NoteEditorProps } from '@/components/NoteEditor';
+
+const EditNotePage = () => {
+  const params = useParams();
+
+  const editorProps: NoteEditorProps = {
+    noteId: params.id
+  };
+
+  return (
+    <>
+      <AppHeader
+        title="Edit Note"
+        leftSection={<Link href="/" className="button-link">Back</Link>} />
+      <AppMain>
+        <NoteEditor {...editorProps} />
+      </AppMain>
+    </>
+  );
+};
+
+export default EditNotePage;
